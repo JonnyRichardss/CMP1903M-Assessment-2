831c5dc3435206dee8c8698ce1af2af1be1f672a
Jonny <unconfigured@null.spigotmc.org>
2023-03-25 05:04:12 +0000

    Add project files.

---
+++ CMP1903M Assessment 2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CMP1903M_Assessment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
