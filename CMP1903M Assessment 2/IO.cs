a6b9ed7c4ef047836d9a600b5fecbfb263239a29
Jonny <unconfigured@null.spigotmc.org>
2023-04-28 20:36:04 +0100

    Added Statistics outputting

---
+++ CMP1903M Assessment 2/IO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_2
+{
+    internal static class IO
+    {
+        private static string FilePath = "Statistics.txt";
+        public static void LoadStats()
+        {
+            try
+            {
+                string FileText = File.ReadAllText(FilePath);
+                Statistics.AllValues = FileText;
+            }
+            catch (Exception e ) when (e is FormatException || e is FileNotFoundException)
+            {
+                Statistics.AllValues = "0,0,0,0";
+                SaveStats();
+            }
+        }
+        public static void SaveStats()
+        {
+            string stats = Statistics.AllValues;
+            File.WriteAllText(stats, FilePath);
+        }
+    }
+}
---
+++ CMP1903M Assessment 2/Statistics.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_Assessment_2
+{
+    public static class Statistics
+    {
+        public static int total3Card;
+        public static int total5Card;
+
+        public static int correct3Card;
+        public static int correct5Card;
+
+        public static string AllValues
+        {
+            get => String.Format("{0},{1},{2},{3}", total3Card, total5Card, correct3Card, correct5Card);
+            set
+            {
+                if (!CheckFormat(value))
+                {
+                    throw new FormatException("Statistics values not in correct format!");
+                }
+                string[] values = value.Split(",");
+                total3Card = int.Parse(values[0]);
+                total5Card = int.Parse(values[1]);
+                correct3Card = int.Parse(values[2]);
+                correct5Card = int.Parse(values[3]);
+            }
+        }
+        static Statistics()
+        {
+            IO.LoadStats();
+        }
+
+        public static bool CheckFormat(string storedValues)
+        {
+            string[] values = storedValues.Split(",");
+            if (values.Length != 4)
+            {
+                return false;
+            }
+            foreach (string s in values)
+            {
+                if (!int.TryParse(s,out _))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
